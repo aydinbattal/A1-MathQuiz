d66a07e5157976771292bcb3a9515b1323aaf58e
Aydin Battal <abattal35@gmail.com>
2022-02-07 00:03:39 -0500

    Implemented Question data class.

---
+++ app/src/main/java/com/example/a1_mathquiz/entities/Question.kt
@@ -0,0 +1,9 @@
+package com.example.a1_mathquiz.entities
+
+/**
+ * A1-MathQuiz created by aydin
+ * student ID : 991521740
+ * on 2022-02-06 */
+data class Question(val questionText:String, val answer:Int, val choices:List<Int>){
+
+}
